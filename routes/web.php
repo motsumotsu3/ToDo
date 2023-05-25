d1d4aa3c1597559f6dbcb09c6c6ec63e5154bee8
Nryoya <ryoooya.r@gmail.com>
2023-05-25 14:39:46 +0900

    認証機能の追加

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\FolderController;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,15 +17,19 @@ use App\Http\Controllers\FolderController;
 |
 */
 
-Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::middleware(['auth'])->group(function () {
+    Route::get('/', [HomeController::class, 'index'])->name('home');
 
-Route::get('/folders/{id}/tasks', [TaskController::class, 'index'])->name('tasks.index');
+    Route::get('/folders/{id}/tasks', [TaskController::class, 'index'])->name('tasks.index');
 
-Route::get('/folders/create', [FolderController::class, 'showCreateForm'])->name('folders.create');
-Route::post('/folders/create', [FolderController::class, 'create']);
+    Route::get('/folders/create', [FolderController::class, 'showCreateForm'])->name('folders.create');
+    Route::post('/folders/create', [FolderController::class, 'create']);
 
-Route::get('/folders/{id}/tasks/create', [TaskController::class, 'ShowCreateForm'])->name('tasks.create');
-Route::post('folders/{id}/tasks/create', [TaskController::class, 'create']);
+    Route::get('/folders/{id}/tasks/create', [TaskController::class, 'ShowCreateForm'])->name('tasks.create');
+    Route::post('folders/{id}/tasks/create', [TaskController::class, 'create']);
 
-Route::get('/folders/{id}/tasks/{task_id}/edit', [TaskController::class, 'showEditForm'])->name('tasks.edit');
-Route::post('/folders/{id}/tasks/{task_id}/edit', [TaskController::class, 'edit']);
+    Route::get('/folders/{id}/tasks/{task_id}/edit', [TaskController::class, 'showEditForm'])->name('tasks.edit');
+    Route::post('/folders/{id}/tasks/{task_id}/edit', [TaskController::class, 'edit']);
+});
+
+Auth::routes();
