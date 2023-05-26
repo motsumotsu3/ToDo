cf274064a158a6882a816a55a732a0cad4c3ba85
Nryoya <ryoooya.r@gmail.com>
2023-05-26 12:00:19 +0900

    不具合の修正

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -59,7 +59,7 @@ class TaskController extends Controller
         $folder->tasks()->save($task);
 
         return redirect()->route('tasks.index', [
-            'id' => $folder->id,
+            'folder' => $folder->id,
         ]);
     }
 
